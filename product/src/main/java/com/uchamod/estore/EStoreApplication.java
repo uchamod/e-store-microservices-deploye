928c77e37cf17cae93b7e2e1736712c6ae9fa254
chamod udara <uchamod52@gmail.com>
2025-10-10 22:15:10 +0530

    fix test issue

--- product/src/main/java/com/uchamod/estore/EStoreApplication.java
+++ product/src/main/java/com/uchamod/estore/EStoreApplication.java
@@ -3,7 +3,7 @@ package com.uchamod.estore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
+//application for users
 @SpringBootApplication
 @EnableConfigurationProperties
 public class EStoreApplication {
