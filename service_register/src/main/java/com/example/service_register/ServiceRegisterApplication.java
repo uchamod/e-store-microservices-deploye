c75ccdecb94dab888e5ae9209cb7dd77190376a6
chamod udara <uchamod52@gmail.com>
2025-10-12 21:56:45 +0530

    add deploye file

--- service_register/src/main/java/com/example/service_register/ServiceRegisterApplication.java
+++ service_register/src/main/java/com/example/service_register/ServiceRegisterApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//eureka service discovery
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegisterApplication {
