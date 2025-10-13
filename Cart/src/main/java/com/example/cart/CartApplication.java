23308e6d66ac0a0dfe670b95deff02e1a27b068f
chamod udara <uchamod52@gmail.com>
2025-10-13 19:57:21 +0530

    reinlilize services

--- Cart/src/main/java/com/example/cart/CartApplication.java
+++ Cart/src/main/java/com/example/cart/CartApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+//cart service
 @SpringBootApplication
 @EnableFeignClients
 public class CartApplication {
