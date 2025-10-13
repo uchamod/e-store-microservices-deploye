95c5a34d7c65943b8f1dc2b56f4503ed9cca65bd
chamod udara <uchamod52@gmail.com>
2025-10-13 20:21:53 +0530

    repackage order

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//order service
+//Order service
 @SpringBootApplication
 @EnableFeignClients
 public class OrderApplication {
